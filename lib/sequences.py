c6679e3182fbadcaa168f00c6678535fcc188faf
Stuart Forrester <stforrester@gmail.com>
2025-01-02 00:08:43 -0600

    completes fibonacci challenge and passes all tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+import ipdb
 
 def print_fibonacci(length):
-    pass
+    fibonacci = [value * 0 for value in range(length)]
+    if len(fibonacci) > 2:
+        fibonacci[1] = 1
+        i = 2
+        while i < length:
+            fibonacci[i] = fibonacci[i-1] + fibonacci[i-2]
+            i += 1
+    elif len(fibonacci) == 2:
+        fibonacci[1] = 1
+    print(fibonacci)
+
+# print_fibonacci(6)
+# ipdb.set_trace()
